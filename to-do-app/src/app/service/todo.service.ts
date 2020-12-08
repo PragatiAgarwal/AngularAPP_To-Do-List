add7a2da4faf302a3b28b54a2ad77766192770f6
Agarwal <Pragati.A@dell.com>
2020-12-08 17:34:38 +0530

    Defining CRUD operations in service file

--- to-do-app/src/app/service/todo.service.ts
+++ to-do-app/src/app/service/todo.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from '../model/Todo'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todos : Todo [];
+
+  constructor() {
+
+    this.todos = [ {
+    id: '111',
+    title: 'Complete Java course',
+    isComplete: false,
+    date : new Date(),
+
+    } ]
+
+    this.todos = [ {
+      id : '222',
+      title : 'Complete JavaScript course',
+      isComplete : false,
+      date : new Date(),
+  
+      } ]
+
+      this.todos = [ {
+        id : '333',
+        title : 'Complete React course',
+        isComplete : true,
+        date : new Date(),
+    
+        } ]
+
+   }
+
+   //Defining CRUD operations
+
+   //1. READ
+   getTodos () {    
+     return of(this.todos);
+   }
+
+   //2. Create
+   addTodo (todo : Todo) {
+    this.todos.push(todo);
+   }
+
+   //3. Update
+   changeStatus (todo : Todo){
+     this.todos.map( (singleTodo) => {
+       if(todo.id === singleTodo.id){
+         todo.isComplete = !todo.isComplete;
+       }
+     })
+   }
+
+   //4. Delete
+   deleteTodo (todo : Todo) {
+     const todoIndex = this.todos.findIndex(
+       (currentObj) => currentObj.id === todo.id
+     )
+   }
+
 }
